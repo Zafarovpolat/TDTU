f89eec1d96d63eba3341912ec775604d6d71ad42
Zafarov Polat <atuin59354081@gmail.com>
2024-02-11 20:33:43 +0500

    Header is done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-let modalBtn = document.getElementById("modalBtn")
+let modalBtn = document.querySelectorAll("#modalBtn")
 let modalWindow = document.querySelector(".modalcover")
 let modalClose = document.getElementById("modalClose")
 let modalBcg = document.querySelector(".modalbcg")
 let modalWrapper = document.querySelector(".modal")
 
-modalBtn.addEventListener('click', () => {
-    modalWindow.style.display = "flex"
-    modalBcg.style.animationName = "modal-on"
-    modalWrapper.style.animationName = "modal-on"
+modalBtn.forEach((el) => {
+    el.addEventListener('click', () => {
+        modalWindow.style.display = "flex"
+        modalBcg.style.animationName = "modal-on"
+        modalWrapper.style.animationName = "modal-on"
+    })
 })
 
 modalClose.addEventListener('click', () => {
@@ -23,5 +25,21 @@ modalBcg.addEventListener('click', () => {
     modalWrapper.style.animationName = "modal-off"
     setTimeout(() => {
         modalWindow.style.display = "none"
+    }, 300)
+})
+
+let burgerOpen = document.getElementById("burgerBtn")
+let burgerMenu = document.querySelector(".header__menu")
+let burgerClose = document.querySelector(".header__menu-close")
+
+burgerOpen.addEventListener('click', () => {
+    burgerMenu.style.display = "flex"
+    burgerMenu.style.animationName = "burger-on"
+})
+
+burgerClose.addEventListener('click', () => {
+    burgerMenu.style.animationName = "burger-off"
+    setTimeout(() => {
+        burgerMenu.style.display = "none"
     }, 300)
 })
