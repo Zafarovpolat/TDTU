a9d2bf9ba44a4396b90bbe9a4a73176ff9e44d1b
Zafarov Polat <atuin59354081@gmail.com>
2024-03-13 19:31:34 +0500

    FINALLY ALL IS DONE!!!

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -28,4 +28,68 @@ const swiper = new Swiper(".mySwiper", {
         },
     }
 
-}); 
+});
+
+const swiperTeachersPhysic = new Swiper(".teachers-physic", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            navigation: {
+                enabled: false
+            }
+        },
+        670: {
+            slidesPerView: 2,
+        },
+        1030: {
+            slidesPerView: 3,
+        },
+        1280: {
+            slidesPerView: 4,
+        },
+    }
+
+});
+
+const swiperTeachersMath = new Swiper(".teachers-math", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            navigation: {
+                enabled: false
+            }
+        },
+        670: {
+            slidesPerView: 2,
+        },
+        1030: {
+            slidesPerView: 3,
+        },
+        1280: {
+            slidesPerView: 4,
+        },
+    }
+
+});
